606328f2dfc2e069dcf0e1e3df80057c702692f1
SubhadeepGarai2006 <subhadeep777sayan@gmail.com>
2026-03-08 12:46:39 +0530

    routing setup

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,16 @@ import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
+
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
+  <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
@@ -21,6 +25,7 @@ root.render(
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
     </Routes>
+    <Footer />
     
   </BrowserRouter>
 );
--- src/landing_page/home/HomePage.jsx
+++ src/landing_page/home/HomePage.jsx
@@ -5,25 +5,16 @@ import Stats from './Stats';
 import Pricing from './pricing';
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-
-
 
 function HomePage() {
     return ( 
         <>
-        <Navbar />
         <Hero />
         <Awards />
         <Stats />
         <Pricing />
         <Education />
         <OpenAccount />
-        <Footer />
-
-
-        
         </>
      );
 }
